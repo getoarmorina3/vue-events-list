70b7e19b06b44d931379f75a0208c4852c8d4cc2
getoarmorina3 <getoaarm1@gmail.com>
2025-01-07 02:21:59 +0100

    Initial Commit

---
+++ src/views/EventListView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
+
+const events = ref(null)
+
+onMounted(() => {
+  // get events from mock db when component is created
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <!-- <EventCard v-for="event in events" :key="event.id" :event="event" /> -->
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
